7d1f231a0219a6e104a7cd6a6423f5c123a474da
Remco <remcodebruin@gmx.net>
2014-05-08 16:44:18 +0200

    fix hardcoded resolution

--- core/vision/balloon.py
+++ core/vision/balloon.py
@@ -7,11 +7,8 @@ import SimpleCV
 import cv2
 import numpy as np
 import debug as db
-width = 640
-height = 480
-#cv2.namedWindow("test")
-whichColor = 'red'
 
+whichColor = 'red'
 
 
 def find(cam, color):
@@ -23,15 +20,10 @@ def find(cam, color):
     while True:#disp.isNotDone():
         frame = cam.getImage()
         median = frame.medianFilter(3)
-        #redOnly(median).threshold(40).show()
         dir = getDirection(median, whichColor)
         print dir
         median.drawText(dir, x=-10, color=SimpleCV.Color.CRIMSON, fontsize=84)
         db.showImg(median)  # show() does not work correct
-        #cv2.imshow("test1", greenOnly(median).getNumpyCv2())
-        #cv2.imshow("test2", redOnly(median).getNumpyCv2())
-        #a = frame.drawText("works 1234, 1234")
-
 
 def greenOnly(value):
     return value.hueDistance(SimpleCV.Color.GREEN, minsaturation=77, minvalue=93).threshold(80).invert()
@@ -43,6 +35,9 @@ def blueOnly(img):
     return img.hueDistance(SimpleCV.Color.BLUE, minsaturation=70, minvalue=150).threshold(90).invert()
 
 def getDirection(img, color):
+    width = img.width
+    height = img.height
+
     if color == 'blue':
         filtered = blueOnly(img)
     elif color == 'red':
