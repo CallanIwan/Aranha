37b0b19f88cc8dd6e081f1b408b4d59f8ca8acc3
Minardus van der Kooi <minardus@gmail.com>
2014-05-13 13:31:50 +0200

    Added camera stream to Core, so the commandcenter can do vision on them and send commands

--- core/main.py
+++ core/main.py
@@ -4,9 +4,19 @@ Main is basically a server who receives commands via Bluetooth or WiFi
 and handles them, FOR NOW IT'S ONLY FOR PROTOTYPING
 """
 
+import sys
 from network import bluetooth_server as bt
+from vision import streamer
 
+# start stream for commandcenter
+stream = streamer.Streamer().start()
+
+# start bluetoothserver for app
 btserve = bt.BluetoothServer().start()
+
+# start PS3Handler thread
 #ps3handler = pshandler.PS3Handler().start()
+
+# exit on keypress
 x = raw_input()
 sys.exit(0)
